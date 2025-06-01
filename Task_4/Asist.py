a20725af21e4128eb798898178560a77cd76ec89
Sivka000 <stepchynv@gmail.com>
2025-06-01 21:49:25 +0300

    v2.7

--- Task_4/Asist.py
+++ Task_4/Asist.py
@@ -3,30 +3,61 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+#Декодер для обробки помилок
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found."
+        except IndexError:
+            return "Please provide the correct number of arguments."
+    return inner
+
 #Додавання контакту в словник
+@input_error
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+#Видалення контакту з словника та попередження про видалення
+@input_error
+def delete_contact(args, contacts):
+    name = args[0]
+    if name in contacts:
+        confirmation = input(f"Are you sure you want to delete a contact '{name}'? (y/n): ").strip().lower()
+        if confirmation in ("y", "yes"):
+            del contacts[name]
+            return f"Contact '{name}' deleted."
+        else:
+            return "Deletion is canceled."
+    else:
+        raise KeyError
+
 #Оновлення номеру
+@input_error
 def change_contact(args, contacts):
     name, phone = args
     if name in contacts:
         contacts[name] = phone
         return "Contact updated."
     else:
-        return "Contact not found."
+        raise KeyError
 
 #Пошук номеру через ім'я
+@input_error
 def get_phone(args, contacts):
     name = args[0]
     if name in contacts:
         return f"{name}'s phone number is {contacts[name]}."
     else:
-        return "Contact not found."
+        raise KeyError
 
 #Список всіх контактів
+@input_error
 def list_contacts(contacts):
     if contacts:
         return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
@@ -37,9 +68,10 @@ def list_contacts(contacts):
 command_list = (
     "Command list:\n"
     "Add (username) (phone) - adds a contact;\n"
+    "Delete, del (username) - delete a contact;\n"
     "Phone (username) - searches for a phone number by username;\n"
     "Change (username) (phone) - stores the new phone number for username in memory;\n"
-    "All - shows all contacts;\n"
+    "All, list - shows all contacts;\n"
     "Close, exit - finishes the work;"
     )
     
@@ -48,8 +80,11 @@ def main():
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        if not user_input: #Обробка помилки при пустому вводі
+            print("Error!\nPlease enter a command.")
+            continue
 
+        command, *args = parse_input(user_input)
         if command in ["close", "exit"]:
             print("Good bye!")
             break
@@ -57,11 +92,13 @@ def main():
             print("How can I help you?")
         elif command == "add":
             print(add_contact(args, contacts))
+        elif command in ["delete", "del"]:
+            print(delete_contact(args, contacts))
         elif command == "change":
             print(change_contact(args, contacts))
         elif command == "phone":
             print(get_phone(args, contacts))
-        elif command == "all":
+        elif command in ["all", "list"]:
             print(list_contacts(contacts))
         elif command in ["help", "command"]:
             print(command_list)
